4d3b1f1ed51ae07e550736d610484328bf28cd02
lb-dl <lb_dl@i.ua>
2020-12-07 23:13:14 +0200

    corrected property active_avatar, save only 1 avatar as active

---
+++ src/account/receivers.py
@@ -0,0 +1,9 @@
+# from account.models import Avatar
+
+# from django.db.models.signals import post_delete
+# from django.dispatch import receiver
+
+
+# @receiver(post_delete, sender=Avatar)
+# def avatar_post_delete_remove_avatar(sender, instance, *args, **kwargs):
+#    instance.file_path.delete()
