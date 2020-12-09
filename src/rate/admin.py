92883ae51f92655a9b7751952c9bfec19f4bf73e
lb-dl <lb_dl@i.ua>
2020-12-09 14:14:37 +0200

    django admin

--- src/rate/admin.py
+++ src/rate/admin.py
@@ -1,3 +1,37 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from rate.models import Rate
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'currency',
+        'source',
+        'buy',
+        'sale',
+        'created'
+    ]
+    list_filter = [
+        'source',
+        'currency',
+        'created',
+    ]
+    readonly_fields = (
+        'source',
+        'currency',
+    )
+
+    actions = None
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.has_perm('rate.full_edit') \
+                or request.user.is_superuser:
+            return ()
+        return super().get_readonly_fields(request, obj=obj)
+
+
+admin.site.register(Rate, RateAdmin)
--- src/rate/models.py
+++ src/rate/models.py
@@ -13,10 +13,19 @@ class Rate(models.Model):
     sale = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        permissions = [
+            ('full_edit', 'This permission allows user to update all available fields in Rate model'),
+        ]
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         key = self.__class__.cache_key()
         cache.delete(key)
+
+    # object into str
+    # def __str__(self):
+    #    return f'{self.id}'
 
     @classmethod
     def cache_key(cls):
